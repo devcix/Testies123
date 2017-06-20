50e605f6cb25f66fe3bd085af2da43ba87b62686
Cixtian Rush <cixtian.rush@gmail.com>
2017-06-20 15:01:04 -0400

    [cix] yet another attempt at getting my Git Repo Fixed

---
+++ scripts/config.js
@@ -0,0 +1,11 @@
+(function(){
+  var express         = require('express');
+  var bodyParser      = require('body-parser');
+  var methodOverride  = require('method-override');
+  var _               = require('lodash');
+  var port            = 3055;
+  var serviceName    = "ScumDog";
+
+  return app = express();
+
+})();
